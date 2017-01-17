063769ae2794f11223f0735e0b9fc004fe444135
Jelle Gerbrandy <github@gerbrandy.com>
2017-01-17 17:16:20 +0100

    make test/dco.js more human-readable

--- test/dco.js
+++ test/dco.js
@@ -1,18 +1,8 @@
 const assertJump = require('./zeppelin-solidity/helpers/assertJump');
 
-function getBallot(tx) {
-    // check that tx.logs[0] is the BallotCreated event we need for the address 
-    assert.equal(tx.logs[0].event, 'BallotCreated')
-    let ballotAddress = tx.logs[0].args.ballotaddress
-    let ballot = Ballot.at(ballotAddress)
-    return ballot
-
-}
 contract('DCO', function(accounts) {
   it("test basic workflow", async function() {
     // set up a dco
-    // (next statement makes transaction return more data)
-    DCO.next_gen = true;
     let reputation = Reputation.deployed()
     let token = MintableToken.deployed()
     let dco = await DCO.new(reputation.address, token.address);
@@ -21,9 +11,13 @@ contract('DCO', function(accounts) {
     // give all reputation to default account
     await reputation.setReputation(accounts[0], 1000);
 
-    // accounts[0] has no tokens at this point
+    // accounts[1] has no tokens at this point
     let oldBalance = await token.balanceOf(accounts[1])
     assert.equal(oldBalance.valueOf(), 0)
+
+    // (next statement makes transaction return more data, and can be remove
+    // once a new version of truffle comes out)
+    DCO.next_gen = true;
 
     // create a ballot to create 1413 tokens and give them to accounts[1]
     let tx = await dco.registerBallotMintTokens(1413, accounts[1])
@@ -73,16 +67,15 @@ contract('DCO', function(accounts) {
 
   })
 
-  //  
-  //   DCO.next_gen = true;
-  // returns logs with the transaction, so we can easily get the ballot address
-  // TODO: remove this when this becomes standard behavior in truffle 
-  // create a ballot to give 1413 tokens to accounts[1]
-  // let tx = await dco.addBallot('minttokens', 1413, accounts[1])
-
-  // check that tx.logs[0] is the BallotCreated event we need for the address 
-  // assert.equal(tx.logs[0].event, 'BallotCreated')
-  // let ballotAddress = tx.logs[0].args.ballotaddress
-  // let ballot = Ballot.at(ballotAddress)
-
 }); 
+
+function getBallot(tx) {
+    // returns a ballot object from the BallotCreated event in the logs of tx
+
+    // first check that tx.logs[0] is the BallotCreated event we need for the address 
+    assert.equal(tx.logs[0].event, 'BallotCreated')
+    let ballotAddress = tx.logs[0].args.ballotaddress
+    let ballot = Ballot.at(ballotAddress)
+    return ballot
+
+}
