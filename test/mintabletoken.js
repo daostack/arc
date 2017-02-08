4bda9feb6fc35adcda7444ff30569808e3eb9fa1
Jelle Gerbrandy <github@gerbrandy.com>
2017-02-08 12:39:22 +0100

    restore some token tests

---
+++ test/mintabletoken.js
@@ -0,0 +1,37 @@
+contract('Test MintableToken', function(accounts) {
+    it("should mint tokens to owner account", async function() {
+        let owner, totalSupply, userSupply
+        let token = MintableToken.deployed();
+        totalSupply = await token.totalSupply();
+        owner = await token.owner()
+        userSupply = await token.balanceOf(owner)
+        assert.equal(totalSupply, 0);
+        assert.equal(userSupply, 0);
+
+        await token.mint(1000, owner)
+        totalSupply = await token.totalSupply();
+        userSupply = await token.balanceOf(owner)
+        assert.equal(totalSupply, 1000);
+        assert.equal(userSupply, 1000);
+
+        await token.mint(1300, accounts[2])
+        totalSupply = await token.totalSupply();
+        userSupply = await token.balanceOf(accounts[2])
+        assert.equal(totalSupply, 2300);
+        assert.equal(userSupply, 1300);
+     
+    });
+
+    it("should allow minting tokens only by owner", async function() {
+        let token = MintableToken.deployed();
+        let owner = await token.owner()
+        let totalSupply = await token.totalSupply();
+
+        // the next call fails silently
+        await token.mint(1000, owner, {'from': accounts[1]})
+
+        // and so the supply of tokens should remain unchanged
+        let newSupply = await token.totalSupply();
+        assert.equal(totalSupply.valueOf(), newSupply.valueOf())
+    });
+});
---
+++ test/token.js
@@ -0,0 +1,21 @@
+contract('Test Token', function(accounts) {
+    it("should put 10000 Coins in the first account", async function() {
+        let token = MintableToken.deployed();
+        let balance = await token.balanceOf.call(accounts[0])
+        assert.equal(balance.valueOf(), 0)
+    });
+
+    it("should be owned by the creator account", async function() {
+        let token = MintableToken.deployed();
+        let owner = await token.owner();
+        assert.equal(owner, accounts[0]);
+    });
+
+    it("should be killable", async function() {
+        // we only test that the function actually exists
+        // "real" tests are in zeppelin-solidity/Killable.js
+        let token = MintableToken.deployed();
+        let txnhash = await token.kill();
+        assert.isOk(txnhash);
+    });
+});
