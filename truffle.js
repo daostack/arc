fcfce8af4c2908b3d11942034acfd4cbc4fa02ed
Elan Perach <elanperach@gmail.com>
2017-03-21 10:51:33 +0200

    update truffle.js to make migrations work

--- truffle.js
+++ truffle.js
@@ -1,31 +1,9 @@
 module.exports = {
-  build: {
-    "index.html": "index.html",
-    "token.html": "token.html",
-    "token.js": [
-      "javascripts/token.js"
-    ],
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  },
-  rpc: {
-    host: "localhost",
-    port: 8545
-  },
   networks: {
-    "live": {
-        network_id: 1 // Ethereum public network
-    },
-    "morden": {
-        network_id: 2 // Official Ethereum test network
-    },
-    "development": {
-        network_id: "default"
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   }
 };
