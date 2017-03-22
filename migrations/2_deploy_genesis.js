626f77b5d1723a50826a60b5d96a7374a1a6b686
Elan Perach <elanperach@gmail.com>
2017-03-22 16:24:41 +0200

    return genesis controller

--- migrations/2_deploy_genesis.js
+++ migrations/2_deploy_genesis.js
@@ -3,6 +3,10 @@ var GenesisScheme = artifacts.require("./schemes/GenesisScheme.sol");
 
 module.exports = function(deployer) {
 	deployer.deploy(SimpleVote).then(function() {
-	  return deployer.deploy(GenesisScheme, 'AdamCoin', 'ADM', ['0x359a4c49ba06cef000441ACC65EC6813c4097541'], [45], [10], SimpleVote.address);
+	  deployer.deploy(GenesisScheme, 'AdamCoin', 'ADM', ['0x359a4c49ba06cef000441ACC65EC6813c4097541'], [45], [10], SimpleVote.address).then(function() {
+
+			return GenesisScheme.deployed().then(function(instance) { instance.controller.call().then(function(cntrl) { console.log(cntrl); }); });
+
+	  });
 	});
 };
