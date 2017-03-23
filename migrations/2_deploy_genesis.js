842fd2143e0976f6f4f47818e81a852a39743c9b
leviadam <leviadam@gmail.com>
2017-03-23 08:17:40 +0200

    Working migrate script for testrpc

--- migrations/2_deploy_genesis.js
+++ migrations/2_deploy_genesis.js
@@ -1,8 +1,52 @@
 var SimpleVote = artifacts.require('./SimpleVote.sol');
 var GenesisScheme = artifacts.require("./schemes/GenesisScheme.sol");
+var OrganizationsBoard = artifacts.require("./schemes/OrganizationsBoard.sol");
+var Controller = artifacts.require("./schemes/controller/Controller.sol");
+var Reputation = artifacts.require("./schemes/controller/Reputation.sol");
+var SimpleVoteInst;
+var GenesisSchemeInst;
+var ControllerInst;
+var OrganizationsBoardInst;
+var ReputationInst;
 
 module.exports = function(deployer) {
-	deployer.deploy(SimpleVote).then(function() {
-	  return deployer.deploy(GenesisScheme, 'AdamCoin', 'ADM', ['0x359a4c49ba06cef000441ACC65EC6813c4097541'], [45], [10], SimpleVote.address);
+	deployer.deploy(SimpleVote).then(function (){
+		return SimpleVote.deployed();
+	}).then(function(inst) {
+		SimpleVoteInst = inst;
+	}).then(function() {
+		return deployer.deploy(GenesisScheme, 'AdamCoin', 'ADM', [web3.eth.accounts[0]], [100], [10], SimpleVoteInst.address);
+	}).then(function() {
+		return GenesisScheme.deployed();
+	}).then(function(inst) {
+		GenesisSchemeInst = inst;
+		return GenesisSchemeInst.collectFoundersShare( { from: web3.eth.accounts[0] });
+	}).then(function() {
+		return GenesisSchemeInst.controller();
+	}).then(function (contAddrss) {
+		return Controller.at(contAddrss);
+	}).then(function (inst){
+		ControllerInst = inst;
+	}).then(function (){
+		return deployer.deploy(OrganizationsBoard, ControllerInst.address, 5, 'AdamDAO');
+	}).then(function () {
+		return OrganizationsBoard.deployed();
+	}).then(function(inst) {
+		OrganizationsBoardInst = inst;
+		return GenesisSchemeInst.proposeScheme(OrganizationsBoardInst.address, { from: web3.eth.accounts[0] });
+	}).then(function() {
+		return GenesisSchemeInst.voteScheme(OrganizationsBoardInst.address, true, { from: web3.eth.accounts[0] });
+	}).then(function() {
+		return ControllerInst.schemes(OrganizationsBoardInst.address);
+	}).then(function(isListed) {
+		console.log('isListed', isListed);
+	}).then(function() {
+		return ControllerInst.nativeReputation();
+	}).then(function(repAddrss) {
+		return Reputation.at(repAddrss)
+	}).then(function(inst) {
+		ReputationInst = inst;
+		return ReputationInst.reputationOf(web3.eth.accounts[0])
 	});
+
 };
