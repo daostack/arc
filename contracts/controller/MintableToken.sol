664ebee945a9397285ac98dbff54650d198bf43c
leviadam <leviadam@gmail.com>
2017-04-16 01:51:40 +0300

    Changed burn to old model. Every scheme can still burn for everyone. Will change after demo to something safer.

--- contracts/controller/MintableToken.sol
+++ contracts/controller/MintableToken.sol
@@ -11,8 +11,7 @@ contract MintableToken is StandardToken, Ownable, Killable {
 
     uint public decimals = 18;
 
-    event Mint(address indexed to, uint value);
-    event Burn(address indexed from, uint value);
+    event Mint(address indexed to, int256 value);
 
     function MintableToken( string _name, string _symbol ) {
         name = _name;
@@ -23,27 +22,18 @@ contract MintableToken is StandardToken, Ownable, Killable {
     function mint(int256 _amount, address _to) onlyOwner returns (bool) {
     	// create new tokens and add them to the given account
     	uint absAmount;
-    	if( _amount < 0 && _to != owner ) throw;
+    	/*if( _amount < 0 && _to != owner ) throw; // Allowing burn for everyone, just for the demo.*/
     	if( _amount >= 0 ) {
         	absAmount = uint(_amount);
             totalSupply = safeAdd(totalSupply, absAmount);
             balances[_to] = safeAdd(balances[_to], absAmount);
-            Mint(_to, uint(_amount));
-        }
-        else {
-            absAmount = uint((-1)*_amount);
-            totalSupply = safeSub(totalSupply, absAmount);
-            balances[_to] = safeSub(balances[_to], absAmount);
-        }
-        return true;
-    }
-
-    function burn(uint256 _amount, address _from) onlyOwner returns (bool) {
-      // Burn tokens of a given account
-      if ( balances[_from] < _amount) throw;
-      balances[_from] = safeSub(balances[_from], _amount);
-      totalSupply = safeSub(totalSupply, _amount);
-      Burn(_from, _amount);
+      }
+      else {
+          absAmount = uint((-1)*_amount);
+          totalSupply = safeSub(totalSupply, absAmount);
+          balances[_to] = safeSub(balances[_to], absAmount);
+      }
+      Mint(_to, _amount);
       return true;
     }
 }
