0d5a452d8b77e8c34dbd802e35e8d445c1422206
leviadam <leviadam@gmail.com>
2017-06-01 03:18:21 +0300

    Updating truffle.json. Making sure babel is pre-compiling before tests.

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+require("babel-polyfill");
+require("babel-register")({
+  "presets": ["es2015"],
+  "plugins": ["syntax-async-functions","transform-regenerator"]
+});
+
 module.exports = {
   networks: {
     development: {
