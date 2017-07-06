46963d2018784c6a3c349a39cc82d43f18dc55da
leviadam <leviadam@gmail.com>
2017-07-06 16:33:51 +0300

    Using our MintableToken and not zeppelin to not break tests.

--- contracts/controller/DAOToken.sol
+++ contracts/controller/DAOToken.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.11;
 
-import "zeppelin-solidity/contracts/token/MintableToken.sol";
+/*import "zeppelin-solidity/contracts/token/MintableToken.sol";*/ // ToDo, Build on zeppelin contrcat.
+import "./MintableToken.sol";
 import "zeppelin-solidity/contracts/SafeMath.sol";
 
 /**
