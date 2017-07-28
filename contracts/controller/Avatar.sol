da7c78d7b07c55286f2a12564271b996d42fb917
Jelle Gerbrandy <github@gerbrandy.com>
2017-07-28 11:22:10 +0200

    fix linter errors in avatar.sol

--- contracts/controller/Avatar.sol
+++ contracts/controller/Avatar.sol
@@ -6,24 +6,26 @@ import "./MintableToken.sol";
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "zeppelin-solidity/contracts/token/StandardToken.sol";
 
+
 contract ActionInterface {
-    function action( uint _param ) returns(bool);
+    function action(uint _param) returns(bool);
 }
+
 
 /**
  * @title An Avatar holds tokens, reputation and ether for a controller
  */
 contract Avatar is Ownable {
-    bytes32         public   orgName;
-    MintableToken   public   nativeToken;
-    Reputation      public   nativeReputation;
+    bytes32 public orgName;
+    MintableToken public nativeToken;
+    Reputation public nativeReputation;
 
-    event GenericAction( address indexed _action, uint _param );
-    event SendEther( uint _amountInWei, address indexed _to );
+    event GenericAction(address indexed _action, uint _param);
+    event SendEther(uint _amountInWei, address indexed _to);
     event ExternalTokenTransfer(address indexed _externalToken, address indexed _to, uint _value);
     event ExternalTokenTransferFrom(address indexed _externalToken, address _from, address _to, uint _value);
     event ExternalTokenApprove(StandardToken indexed _externalToken, address _spender, uint _value);
-    event TokenDisapprove(address _token, uint _value );
+    event TokenDisapprove(address _token, uint _value);
     event ReceiveEther(address indexed _sender, uint _value);
 
     /**
@@ -39,10 +41,11 @@ contract Avatar is Ownable {
     /**
      * @dev ???
      */
-    function genericAction( ActionInterface _action, uint _param ) // TODO discuss name
-    onlyOwner returns(bool){
-        GenericAction( _action, _param );
+    function genericAction(ActionInterface _action, uint _param)
+        onlyOwner returns(bool)
+    {
         return _action.delegatecall(bytes4(sha3("action(uint256)")), _param);
+        GenericAction(_action, _param);
     }
 
     /**
@@ -51,37 +54,43 @@ contract Avatar is Ownable {
      * @param _to send the ethers to this address
      * @return bool which represents success
      */
-    function sendEther( uint _amountInWei, address _to ) onlyOwner returns(bool) {
-      _to.transfer(_amountInWei);
+    function sendEther(uint _amountInWei, address _to) onlyOwner returns(bool) {
+        _to.transfer(_amountInWei);
         SendEther(_amountInWei, _to);
         return true;
     }
 
-
     function externalTokenTransfer(StandardToken _externalToken, address _to, uint _value)
-      onlyOwner returns(bool) {
-      ExternalTokenTransfer(_externalToken, _to, _value);
-      _externalToken.transfer(_to, _value);
-      return true;
+        onlyOwner returns(bool)
+    {
+        _externalToken.transfer(_to, _value);
+        ExternalTokenTransfer(_externalToken, _to, _value);
+        return true;
     }
 
-    function externalTokenTransferFrom(StandardToken _externalToken, address _from, address _to, uint _value)
-      onlyOwner returns(bool) {
-      ExternalTokenTransferFrom(_externalToken, _from, _to, _value);
-      return true;
-        _externalToken.transferFrom( _from, _to, _value );
+    function externalTokenTransferFrom(
+        StandardToken _externalToken,
+        address _from,
+        address _to,
+        uint _value)
+        onlyOwner returns(bool)
+    {
+        _externalToken.transferFrom(_from, _to, _value);
+        ExternalTokenTransferFrom(_externalToken, _from, _to, _value);
+        return true;
     }
 
     function externalTokenApprove(StandardToken _externalToken, address _spender, uint _value)
-      onlyOwner returns(bool) {
-      ExternalTokenApprove(_externalToken, _spender, _value);
+        onlyOwner returns(bool)
+    {
         _externalToken.approve(_spender, _value);
+        ExternalTokenApprove(_externalToken, _spender, _value);
         return true;
     }
 
     function tokenDisapprove(StandardToken _token, uint _value ) onlyOwner returns(bool) {
+        _token.transferFrom(msg.sender,msg.sender, _value);
         TokenDisapprove(_token, _value);
-        _token.transferFrom( msg.sender,msg.sender, _value);
         return true;
     }
 
