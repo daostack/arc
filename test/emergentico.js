2e25f60b49e7ec53ed0f6a68d1aee1bb657c8839
Shabtai Dvir <shay.mail@gmail.com>
2017-09-05 19:31:39 +0300

    comment error

--- test/emergentico.js
+++ test/emergentico.js
@@ -82,120 +82,120 @@ contract("EmergentICO", function(accounts){
     // Checking rate is the same up to rounding error:
     assert(Math.abs(averageRate18Digits - averageRateCalc18Digits)/averageRate18Digits < 10**(-8));
   });
-  //
-  // it("Only admin can halt and resume the ICO", async function(){
-  //
-  //   await setupEmergentICO();
-  //
-  //   await newICO.haltICO();
-  //   // ICO should be inactive - Admin halted
-  //   assert.equal(await newICO.isActive(), false);
-  //   newICO.resumeICO();
-  //   // ICO should be active - Admin resumed
-  //   assert.equal(await newICO.isActive(), true);
-  //
-  //   // only the admin can halt the ICO
-  //   try {
-  //       await newICO.haltICO({from: accounts[1]});
-  //       throw 'an error'; // make sure that an error is thrown
-  //   } catch(error) {
-  //       helpers.assertVMException(error);
-  //   }
-  //   // ICO should be still active - The halt request sent from non admin account
-  //   assert.equal(await newICO.isActive(), true);
-  //
-  //   // Halting the ICO:
-  //   await newICO.haltICO();
-  //   // only the admin can resume the ICO
-  //   try {
-  //       await newICO.resumeICO({from: accounts[1]});
-  //       throw 'an error'; // make sure that an error is thrown
-  //   } catch(error) {
-  //       helpers.assertVMException(error);
-  //   }
-  //   // ICO should still be halted:
-  //   assert.equal(await newICO.isActive(), false);
-  // });
-  //
-  // it("Try donating when ICO is halted", async function(){
-  //   await setupEmergentICO();
-  //
-  //   // Halting the ICO:
-  //   await newICO.haltICO();
-  //
-  //   // Try to donate:
-  //   try {
-  //     await web3.eth.sendTransaction({
-  //       from: accounts[1],
-  //       to: newICO.address,
-  //       value: web3.toWei(2, "ether"),
-  //     });
-  //     throw 'an error'; // make sure that an error is thrown
-  //   } catch(error) {
-  //       helpers.assertVMException(error);
-  //   }
-  //
-  //   // Checking contract variables:
-  //   assert.equal(await newICO.totalReceived(), 0);
-  //   assert.equal(await newICO.donationCounter(), 0);
-  // });
-  //
-  // it("Try donating below minimum", async function(){
-  //   await setupEmergentICO();
-  //
-  //
-  //   // Try to donate:
-  //   try {
-  //     await web3.eth.sendTransaction({
-  //       from: accounts[1],
-  //       to: newICO.address,
-  //       value: web3.toWei(0.5, "ether"),
-  //     });
-  //     throw 'an error'; // make sure that an error is thrown
-  //   } catch(error) {
-  //       helpers.assertVMException(error);
-  //   }
-  //
-  //   // Checking contract variables:
-  //   assert.equal(await newICO.totalReceived(), 0);
-  //   assert.equal(await newICO.donationCounter(), 0);
-  // });
-  //
-  // it("Single regular donation", async function(){
-  //   await setupEmergentICO();
-  //
-  //   const donationInWei = web3.toWei(2, "ether");
-  //   const targetOriginalBalance = await web3.eth.getBalance(target);
-  //
-  //   // Regular small send:
-  //   await web3.eth.sendTransaction({
-  //     from: accounts[1],
-  //     to: newICO.address,
-  //     value: web3.toWei(2, "ether"),
-  //     gas: 600000
-  //   });
-  //   // Checking contract variables:
-  //   assert.equal(Number(await newICO.totalReceived()), donationInWei);
-  //   assert.equal(Number(await newICO.totalDonated()), donationInWei);
-  //   assert.equal(await newICO.donationCounter(), 1);
-  //   assert.equal(Number(await web3.eth.getBalance(target)), Number(targetOriginalBalance) + Number(donationInWei));
-  // });
-  //
-  // it("Single donation with limit", async function(){
-  //   await setupEmergentICO();
-  //
-  //   const donationInWei = web3.toWei(2, "ether");
-  //   const targetOriginalBalance = await web3.eth.getBalance(target);
-  //
-  //   // Donating:
-  //   await newICO.donate(accounts[2], web3.toWei(5,"ether"), {from: accounts[1], value: donationInWei});
-  //
-  //   // Checking contract variables:
-  //   assert.equal(Number(await newICO.totalReceived()), donationInWei);
-  //   assert.equal(await newICO.totalDonated(), 0);
-  //   assert.equal(await newICO.donationCounter(), 1);
-  //   assert.equal(await Number(web3.eth.getBalance(target)), Number(targetOriginalBalance));
-  // });
+
+  it("Only admin can halt and resume the ICO", async function(){
+
+    await setupEmergentICO();
+
+    await newICO.haltICO();
+    // ICO should be inactive - Admin halted
+    assert.equal(await newICO.isActive(), false);
+    newICO.resumeICO();
+    // ICO should be active - Admin resumed
+    assert.equal(await newICO.isActive(), true);
+
+    // only the admin can halt the ICO
+    try {
+        await newICO.haltICO({from: accounts[1]});
+        throw 'an error'; // make sure that an error is thrown
+    } catch(error) {
+        helpers.assertVMException(error);
+    }
+    // ICO should be still active - The halt request sent from non admin account
+    assert.equal(await newICO.isActive(), true);
+
+    // Halting the ICO:
+    await newICO.haltICO();
+    // only the admin can resume the ICO
+    try {
+        await newICO.resumeICO({from: accounts[1]});
+        throw 'an error'; // make sure that an error is thrown
+    } catch(error) {
+        helpers.assertVMException(error);
+    }
+    // ICO should still be halted:
+    assert.equal(await newICO.isActive(), false);
+  });
+
+  it("Try donating when ICO is halted", async function(){
+    await setupEmergentICO();
+
+    // Halting the ICO:
+    await newICO.haltICO();
+
+    // Try to donate:
+    try {
+      await web3.eth.sendTransaction({
+        from: accounts[1],
+        to: newICO.address,
+        value: web3.toWei(2, "ether"),
+      });
+      throw 'an error'; // make sure that an error is thrown
+    } catch(error) {
+        helpers.assertVMException(error);
+    }
+
+    // Checking contract variables:
+    assert.equal(await newICO.totalReceived(), 0);
+    assert.equal(await newICO.donationCounter(), 0);
+  });
+
+  it("Try donating below minimum", async function(){
+    await setupEmergentICO();
+
+
+    // Try to donate:
+    try {
+      await web3.eth.sendTransaction({
+        from: accounts[1],
+        to: newICO.address,
+        value: web3.toWei(0.5, "ether"),
+      });
+      throw 'an error'; // make sure that an error is thrown
+    } catch(error) {
+        helpers.assertVMException(error);
+    }
+
+    // Checking contract variables:
+    assert.equal(await newICO.totalReceived(), 0);
+    assert.equal(await newICO.donationCounter(), 0);
+  });
+
+  it("Single regular donation", async function(){
+    await setupEmergentICO();
+
+    const donationInWei = web3.toWei(2, "ether");
+    const targetOriginalBalance = await web3.eth.getBalance(target);
+
+    // Regular small send:
+    await web3.eth.sendTransaction({
+      from: accounts[1],
+      to: newICO.address,
+      value: web3.toWei(2, "ether"),
+      gas: 600000
+    });
+    // Checking contract variables:
+    assert.equal(Number(await newICO.totalReceived()), donationInWei);
+    assert.equal(Number(await newICO.totalDonated()), donationInWei);
+    assert.equal(await newICO.donationCounter(), 1);
+    assert.equal(Number(await web3.eth.getBalance(target)), Number(targetOriginalBalance) + Number(donationInWei));
+  });
+
+  it("Single donation with limit", async function(){
+    await setupEmergentICO();
+
+    const donationInWei = web3.toWei(2, "ether");
+    const targetOriginalBalance = await web3.eth.getBalance(target);
+
+    // Donating:
+    await newICO.donate(accounts[2], web3.toWei(5,"ether"), {from: accounts[1], value: donationInWei});
+
+    // Checking contract variables:
+    assert.equal(Number(await newICO.totalReceived()), donationInWei);
+    assert.equal(await newICO.totalDonated(), 0);
+    assert.equal(await newICO.donationCounter(), 1);
+    assert.equal(await Number(web3.eth.getBalance(target)), Number(targetOriginalBalance));
+  });
 
   it("Full scenario 1", async function(){
     await setupEmergentICO();
@@ -275,40 +275,40 @@ contract("EmergentICO", function(accounts){
      assert(Math.abs(collectedTokens - tokensToBeCollected)/tokensToBeCollected < 10**(-8));
   });
 
-  it("Full scenario 2", async function() {
-    await setupEmergentICO();
-
-    // Original data:
-    const period = Number(await newICO.currentClearancePeriod());
-
-    // Donating:
-    await newICO.donate(accounts[1], 0, {from: accounts[1], value: web3.toWei(20, "ether")});
-    await newICO.donate(accounts[3], web3.toWei(98,"ether"), {from: accounts[3], value: web3.toWei(10, "ether")});
-    await newICO.donate(accounts[4], 0, {from: accounts[4], value: web3.toWei(12, "ether")});
-    await newICO.donate(accounts[5], web3.toWei(97.5,"ether"), {from: accounts[5], value: web3.toWei(7, "ether")});
-    await newICO.donate(accounts[5], web3.toWei(97.5,"ether"), {from: accounts[5], value: web3.toWei(7, "ether")});
-
-    // Mining blocks to end of the next period:
-    while(Number(await newICO.currentClearancePeriod()) <= period + 1) {
-        await web3.eth.sendTransaction({
-          from: accounts[0],
-          to: accounts[1],
-          value: web3.toWei(0.1,"ether")
-        });
-    }
-
-    // Checking contract variables:
-    assert.equal(Number(await newICO.totalReceived()), Number(web3.toWei(56, "ether")));
-    assert.equal(await newICO.totalDonated(), Number(web3.toWei(32, "ether")));
-    assert.equal(await newICO.donationCounter(), 5);
-
-    // Compute all previous periods, and initialize current period:
-    for (let cnt=0; cnt<period; cnt++) {
-      let avg = await newICO.averageRateCalc18Digits(0, web3.toWei(38,"ether"));
-      await newICO.setAverageAndTest(cnt, web3.toWei(avg), 5);
-    }
-    const periodInit = await newICO.getIsPeriodInitialized(Number(await newICO.currentClearancePeriod()));
-    assert.equal(periodInit, true);
-  });
+  // it("Full scenario 2", async function() {
+  //   await setupEmergentICO();
+  //
+  //   // Original data:
+  //   const period = Number(await newICO.currentClearancePeriod());
+  //
+  //   // Donating:
+  //   await newICO.donate(accounts[1], 0, {from: accounts[1], value: web3.toWei(20, "ether")});
+  //   await newICO.donate(accounts[3], web3.toWei(98,"ether"), {from: accounts[3], value: web3.toWei(10, "ether")});
+  //   await newICO.donate(accounts[4], 0, {from: accounts[4], value: web3.toWei(12, "ether")});
+  //   await newICO.donate(accounts[5], web3.toWei(97.5,"ether"), {from: accounts[5], value: web3.toWei(7, "ether")});
+  //   await newICO.donate(accounts[5], web3.toWei(97.5,"ether"), {from: accounts[5], value: web3.toWei(7, "ether")});
+  //
+  //   // Mining blocks to end of the next period:
+  //   while(Number(await newICO.currentClearancePeriod()) <= period + 1) {
+  //       await web3.eth.sendTransaction({
+  //         from: accounts[0],
+  //         to: accounts[1],
+  //         value: web3.toWei(0.1,"ether")
+  //       });
+  //   }
+  //
+  //   // Checking contract variables:
+  //   assert.equal(Number(await newICO.totalReceived()), Number(web3.toWei(56, "ether")));
+  //   assert.equal(await newICO.totalDonated(), Number(web3.toWei(32, "ether")));
+  //   assert.equal(await newICO.donationCounter(), 5);
+  //
+  //   // Compute all previous periods, and initialize current period:
+  //   for (let cnt=0; cnt<period; cnt++) {
+  //     let avg = await newICO.averageRateCalc18Digits(0, web3.toWei(38,"ether"));
+  //     await newICO.setAverageAndTest(cnt, web3.toWei(avg), 5);
+  //   }
+  //   const periodInit = await newICO.getIsPeriodInitialized(Number(await newICO.currentClearancePeriod()));
+  //   assert.equal(periodInit, true);
+  // });
 
 });
