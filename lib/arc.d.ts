5fdd3eabff4740373c21ead3399b8ea00429995a
Tibet Sprague <tibet.sprague@gmail.com>
2017-11-07 17:12:45 -0800

    Fix to typescript definitions

--- lib/arc.d.ts
+++ lib/arc.d.ts
@@ -1,7 +1,7 @@
 declare module 'emergent-arc' {
   export class Wallet {
-    static new(password: string, progressCallback: (progress: number) => null) : Wallet;
-    static fromEncrypted(encryptedJSON: string, password: string, progressCallback: (progress: number) => null) : Wallet
+    static new(password: string, progressCallback: (progress: number) => void) : Wallet;
+    static fromEncrypted(encryptedJSON: string, password: string, progressCallback: (progress: number) => void) : Wallet
 
     getEncryptedJSON() : string
     getEtherBalance() : any // TODO return bignumber
