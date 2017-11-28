53f399931b34998f37b0cfb103164839f7b5d596
Doug Kent <github@douglaskent.com>
2017-11-27 23:38:44 -0500

    First stage, just scheme helper methods

--- dist/schemeregistrar.js
+++ dist/schemeregistrar.js
@@ -1,13 +1,15 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.SchemeRegistrar = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _utils = require('./utils.js');
+
+var _settings = require('./settings.js');
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -21,49 +23,139 @@ var SoliditySchemeRegistrar = (0, _utils.requireContract)("SchemeRegistrar");
 var DAOToken = (0, _utils.requireContract)("DAOToken");
 
 var SchemeRegistrar = exports.SchemeRegistrar = function (_ExtendTruffleContrac) {
-  _inherits(SchemeRegistrar, _ExtendTruffleContrac);
+    _inherits(SchemeRegistrar, _ExtendTruffleContrac);
 
-  function SchemeRegistrar() {
-    _classCallCheck(this, SchemeRegistrar);
+    function SchemeRegistrar() {
+        _classCallCheck(this, SchemeRegistrar);
 
-    return _possibleConstructorReturn(this, (SchemeRegistrar.__proto__ || Object.getPrototypeOf(SchemeRegistrar)).apply(this, arguments));
-  }
-
-  _createClass(SchemeRegistrar, [{
-    key: 'setParams',
-    value: async function setParams(params) {
-      return await this._setParameters(params.voteParametersHash, params.voteParametersHash, params.votingMachine);
+        return _possibleConstructorReturn(this, (SchemeRegistrar.__proto__ || Object.getPrototypeOf(SchemeRegistrar)).apply(this, arguments));
     }
-  }, {
-    key: 'getDefaultPermissions',
-    value: function getDefaultPermissions(overrideValue) {
-      return overrideValue || '0x00000003';
-    }
-  }], [{
-    key: 'new',
-    value: async function _new() {
-      var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-      // TODO: provide options to use an existing token or specifiy the new token
-      var defaults = {
-        fee: 0, // the fee to use this scheme
-        beneficiary: (0, _utils.getDefaultAccount)(),
-        tokenAddress: undefined // the address of a token to use
-      };
+    _createClass(SchemeRegistrar, [{
+        key: 'proposeToAddModifyScheme',
+        value: async function proposeToAddModifyScheme() {
+            var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-      var options = dopts(opts, defaults);
 
-      var token = void 0;
-      if (options.tokenAddress == undefined) {
-        token = await DAOToken.new('schemeregistrartoken', 'SRT');
-      } else {
-        token = await DAOToken.at(options.tokenAddress);
-      }
+            var defaults = {
+                /**
+                 * avatar address
+                 */
+                avatar: undefined
+                /**
+                 * scheme address
+                 */
+                , scheme: undefined
+                /**
+                 * scheme identifier, like "SchemeRegistrar" or "SimpleContributionScheme"
+                 */
+                , schemeKey: undefined
+                /**
+                 * scheme parameters
+                 */
+                , schemeParametersHash: undefined
+            };
 
-      contract = await SoliditySchemeRegistrar.new(token.address, options.fee, options.beneficiary);
-      return new this(contract);
-    }
-  }]);
+            var options = dopts(opts, defaults);
 
-  return SchemeRegistrar;
+            if (!options.avatar) {
+                throw new Error("avatar address is not defined");
+            }
+
+            if (!options.scheme) {
+                throw new Error("scheme is not defined");
+            }
+
+            if (!options.schemeKey) {
+                throw new Error("schemeKey is not defined");
+            }
+
+            if (!options.schemeParametersHash) {
+                throw new Error("schemeParametersHash is not defined");
+            }
+
+            var settings = await (0, _settings.getSettings)();
+            var newScheme = await settings.daostackContracts[options.schemeKey].contract.at(options.scheme);
+            var fee = await newScheme.fee();
+            var tokenAddress = await newScheme.nativeToken();
+            // Note that the javascript wrapper "newScheme" we've gotten here is defined in this version of Arc.  If newScheme is 
+            // actually coming from a different version of Arc, then theoretically the permissions could be different from this version.
+            var permissions = Number(newScheme.getDefaultPermissions());
+
+            if (permissions > this.getDefaultPermissions()) {
+                throw new Error("SchemeRegistrar cannot work with schemes having greater permissions than its own");
+            }
+
+            var tx = await this.contract.proposeScheme(options.avatar, options.scheme, options.schemeParametersHash, (permissions & 2) != 0, tokenAddress, fee, true);
+
+            return tx;
+        }
+    }, {
+        key: 'proposeToRemoveScheme',
+        value: async function proposeToRemoveScheme() {
+            var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+
+            var defaults = {
+                /**
+                 * avatar address
+                 */
+                avatar: undefined
+                /**
+                 * scheme address
+                 */
+                , scheme: undefined
+            };
+
+            var options = dopts(opts, defaults);
+
+            if (!options.avatar) {
+                throw new Error("avatar address is not defined");
+            }
+
+            if (!options.scheme) {
+                throw new Error("scheme address is not defined");
+            }
+
+            var tx = await this.contract.proposeToRemoveScheme(options.avatar, options.scheme);
+
+            return tx;
+        }
+    }, {
+        key: 'setParams',
+        value: async function setParams(params) {
+            return await this._setParameters(params.voteParametersHash, params.voteParametersHash, params.votingMachine);
+        }
+    }, {
+        key: 'getDefaultPermissions',
+        value: function getDefaultPermissions(overrideValue) {
+            return overrideValue || '0x00000003';
+        }
+    }], [{
+        key: 'new',
+        value: async function _new() {
+            var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+            // TODO: provide options to use an existing token or specifiy the new token
+            var defaults = {
+                fee: 0, // the fee to use this scheme
+                beneficiary: (0, _utils.getDefaultAccount)(),
+                tokenAddress: undefined // the address of a token to use
+            };
+
+            var options = dopts(opts, defaults);
+
+            var token = void 0;
+            if (options.tokenAddress == undefined) {
+                token = await DAOToken.new('schemeregistrartoken', 'SRT');
+            } else {
+                token = await DAOToken.at(options.tokenAddress);
+            }
+
+            contract = await SoliditySchemeRegistrar.new(token.address, options.fee, options.beneficiary);
+            return new this(contract);
+        }
+    }]);
+
+    return SchemeRegistrar;
 }((0, _utils.ExtendTruffleContract)(SoliditySchemeRegistrar));
--- dist/upgradescheme.js
+++ dist/upgradescheme.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.UpgradeScheme = undefined;
 
@@ -21,51 +21,154 @@ var SolidityUpgradeScheme = (0, _utils.requireContract)("UpgradeScheme");
 var DAOToken = (0, _utils.requireContract)("DAOToken");
 
 var UpgradeScheme = exports.UpgradeScheme = function (_ExtendTruffleContrac) {
-  _inherits(UpgradeScheme, _ExtendTruffleContrac);
+    _inherits(UpgradeScheme, _ExtendTruffleContrac);
 
-  function UpgradeScheme() {
-    _classCallCheck(this, UpgradeScheme);
+    function UpgradeScheme() {
+        _classCallCheck(this, UpgradeScheme);
 
-    return _possibleConstructorReturn(this, (UpgradeScheme.__proto__ || Object.getPrototypeOf(UpgradeScheme)).apply(this, arguments));
-  }
-
-  _createClass(UpgradeScheme, [{
-    key: 'setParams',
-    value: async function setParams(params) {
-      return await this._setParameters(params.voteParametersHash, params.votingMachine);
+        return _possibleConstructorReturn(this, (UpgradeScheme.__proto__ || Object.getPrototypeOf(UpgradeScheme)).apply(this, arguments));
     }
-  }, {
-    key: 'getDefaultPermissions',
-    value: function getDefaultPermissions(overrideValue) {
-      return overrideValue || '0x00000009';
-    }
-  }], [{
-    key: 'new',
-    value: async function _new() {
-      var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-      // TODO: provide options to use an existing token or specifiy the new token
-      var defaults = {
-        fee: 0, // the fee to use this scheme
-        beneficiary: (0, _utils.getDefaultAccount)(),
-        tokenAddress: null // the address of a token to use
-      };
+    _createClass(UpgradeScheme, [{
+        key: 'proposeController',
 
-      var options = dopts(opts, defaults);
 
-      var token = void 0;
-      if (options.tokenAddress == null) {
-        token = await DAOToken.new('schemeregistrartoken', 'SRT');
-        // TODO: or is it better to throw an error?
-        // throw new Error('A tokenAddress must be provided');
-      } else {
-        token = await DAOToken.at(options.tokenAddress);
-      }
+        /*******************************************
+         * proposeController
+         */
+        value: async function proposeController() {
+            var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-      contract = await SolidityUpgradeScheme.new(token.address, options.fee, options.beneficiary);
-      return new this(contract);
-    }
-  }]);
+            var defaults = {
+                /**
+                 * avatar address
+                 */
+                avatar: undefined
+                /**
+                 *  controller address
+                 */
+                , controller: undefined
+            };
 
-  return UpgradeScheme;
+            var options = dopts(opts, defaults);
+
+            if (!options.avatar) {
+                throw new Error("avatar address is not defined");
+            }
+
+            if (!options.controller) {
+                throw new Error("controller address is not defined");
+            }
+
+            var tx = await this.contract.proposeUpgrade(options.avatar, options.controller);
+
+            return tx;
+        }
+
+        /********************************************
+         * proposeUpgradingScheme
+         */
+
+    }, {
+        key: 'proposeUpgradingScheme',
+        value: async function proposeUpgradingScheme() {
+            var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+
+            var defaults = {
+                /**
+                 * avatar address
+                 */
+                avatar: undefined
+                /**
+                 *  upgrading scheme address
+                 */
+                , scheme: undefined
+                /**
+                 * hash of the parameters of the upgrading scheme
+                 */
+                , schemeParametersHash: undefined
+                /**
+                 * address of token that will be used by the upgrading scheme when it is required to pay for something.
+                 * Should be the NativeToken of the new upgrading scheme.
+                 * Only required when fee is non-zero.  Should be the SchemeFee of the upgrading scheme.
+                 */
+                // , tokenAddress: null
+                /**
+                 * fee to charge when fulfilling it's functions
+                 */
+                // , fee: 0
+            };
+
+            var options = dopts(opts, defaults);
+
+            if (!options.avatar) {
+                throw new Error("avatar address is not defined");
+            }
+
+            if (!options.scheme) {
+                throw new Error("scheme is not defined");
+            }
+
+            if (!options.schemeParametersHash) {
+                throw new Error("schemeParametersHash is not defined");
+            }
+
+            // if ((options.fee < 0))
+            // {
+            //   throw new Error("fee cannot be less than 0");
+            // }
+
+            // if ((options.fee > 0) && !options.tokenAddress)
+            // {
+            //   throw new Error("fee is greater than zero but tokenAddress is not defined");
+            // }
+
+            var newScheme = await settings.daostackContracts.UpgradeScheme.contract.at(options.scheme);
+            var fee = await newScheme.fee();
+            var tokenAddress = await newScheme.nativeToken();
+
+            var tx = await this.contract.proposeChangeUpgradingScheme(options.avatar, options.scheme, options.schemeParametersHash, tokenAddress, fee);
+
+            return tx;
+        }
+    }, {
+        key: 'setParams',
+        value: async function setParams(params) {
+            return await this._setParameters(params.voteParametersHash, params.votingMachine);
+        }
+    }, {
+        key: 'getDefaultPermissions',
+        value: function getDefaultPermissions(overrideValue) {
+            return overrideValue || '0x00000009';
+        }
+    }], [{
+        key: 'new',
+        value: async function _new() {
+            var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+            // TODO: provide options to use an existing token or specifiy the new token
+            var defaults = {
+                fee: 0, // the fee to use this scheme
+                beneficiary: (0, _utils.getDefaultAccount)(),
+                tokenAddress: null // the address of a token to use
+            };
+
+            var options = dopts(opts, defaults);
+
+            var token = void 0;
+            if (options.tokenAddress == null) {
+                token = await DAOToken.new('schemeregistrartoken', 'SRT');
+                // TODO: or is it better to throw an error?
+                // throw new Error('A tokenAddress must be provided');
+            } else {
+                token = await DAOToken.at(options.tokenAddress);
+            }
+
+            contract = await SolidityUpgradeScheme.new(token.address, options.fee, options.beneficiary);
+            return new this(contract);
+        }
+    }]);
+
+    return UpgradeScheme;
 }((0, _utils.ExtendTruffleContract)(SolidityUpgradeScheme));
