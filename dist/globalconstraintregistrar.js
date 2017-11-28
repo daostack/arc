eb0c000b5511ad35ff675f67ac73d8956de0c22a
Doug Kent <github@douglaskent.com>
2017-11-28 09:43:02 -0500

    added to helper method support for registering non-arc schemes

--- dist/globalconstraintregistrar.js
+++ dist/globalconstraintregistrar.js
@@ -9,8 +9,6 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 var _utils = require('./utils.js');
 
-var _organization = require('./organization.js');
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
