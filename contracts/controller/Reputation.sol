db077da540f2d0f21a8c4888d95ec5509873d5cc
Unknown <leviadam@gmail.com>
2018-01-19 15:00:09 +0200

    Refactor reputation contract
    1. On negative decreasing more reputation than exists to a user, balnce will be zero instead of throwing.
    2. Adjusted the tests and the gas.
    3. Updated solium.

--- contracts/controller/Reputation.sol
+++ contracts/controller/Reputation.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
-import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 /**
@@ -11,13 +10,21 @@ import "zeppelin-solidity/contracts/math/SafeMath.sol";
  */
 
 contract Reputation is Ownable {
-    using SafeMath for uint;
 
     mapping (address => uint256) balances;
     uint256 public totalSupply;
     uint public decimals = 18;
 
+    // Event indicating minting of reputation to an address.
     event Mint(address indexed to, int256 amount);
+
+    /**
+    * @dev enforce a cap to avoid casting problems
+    */
+    modifier capTotalSupply() {
+        _;
+        require(int(totalSupply) >= 0);
+    }
 
     /**
     * @dev the constructor initiate a reputation system with no supply at all
@@ -37,24 +44,25 @@ contract Reputation is Ownable {
 
     /**
     * @dev adding/reducing reputation of a given address, updating the total supply,
-    * and triggering an event of the operation
+    * and triggering an event of the operation.
     * @param _to the address which we gives/takes reputation amount
     * @param _amount the reputation amount to be added/reduced
     * @return bool which represents a successful of the function
     */
-    function mint(address _to, int256 _amount) public onlyOwner returns (bool) {
+    function mint(address _to, int256 _amount) public onlyOwner capTotalSupply returns (bool) {
         // create new tokens and add them to the given account
-        uint absAmount; // allow to reduce reputation also for non owner
-        if ( _amount >= 0 ) {
-            absAmount = uint(_amount);
-            totalSupply = totalSupply.add(absAmount);
-            balances[_to] = balances[_to].add(absAmount);
+        int amountMinted = _amount;
+
+        if (int(balances[_to]) + _amount >= 0 ) {
+            balances[_to] = uint(int(balances[_to]) + _amount);
+            totalSupply = uint(int(totalSupply) + _amount);
         } else {
-            absAmount = uint((-1)*_amount);
-            totalSupply = totalSupply.sub(absAmount);
-            balances[_to] = balances[_to].sub(absAmount);
+            amountMinted = (-1)*int(balances[_to]);
+            totalSupply -= balances[_to];
+            balances[_to] = 0;
         }
-        Mint(_to, _amount);
+
+        Mint(_to, amountMinted);
         return true;
     }
 
@@ -64,10 +72,14 @@ contract Reputation is Ownable {
     * @param _amount the new reputation amount to be set
     * @return bool which represents a success
     */
-    function setReputation(address _to, uint256 _amount) public onlyOwner returns (bool) {
+    function setReputation(address _to, uint256 _amount) public onlyOwner capTotalSupply returns (bool) {
+        // Require _amount will not overflow on casting:
+        require(int(_amount) > 0);
         // set the balance of _to to _amount
-        totalSupply = (totalSupply.sub(balances[_to])).add(_amount);
+        int amountMinted = int(_amount - balances[_to]);
+        totalSupply = uint(int(totalSupply) + amountMinted);
         balances[_to] = _amount;
+        Mint(_to, amountMinted);
         return true;
     }
 }
