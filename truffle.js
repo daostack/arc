04ab92e7f52305722dbedf09701cc487da7527bf
Adam Levi <leviadam@gmail.com>
2018-01-31 10:21:32 +0200

    Update the different networksId (#286)

--- truffle.js
+++ truffle.js
@@ -6,24 +6,36 @@ require("babel-register")({
 
 module.exports = {
   networks: {
-    development: {
+    live: {
+      network_id: 1,
       host: "localhost",
-      port: 8545,
-      network_id: "*",
+      port: 8546,
       gas: 4543760
     },
     ropsten: {
+      network_id: 3,
       host: "localhost",
       port: 8545,
-      network_id: "*",
+      gas: 4543760
+    },
+    rinkeby: {
+      network_id: 4,
+      host: "localhost",
+      port: 8545,
       gas: 4543760
     },
     kovan: {
+      network_id: 42,
       host: "localhost",
       port: 8545,
-      network_id: "*",
       gas: 4543760
-    }
+    },
+    development: {
+      network_id: "*",
+      host: "localhost",
+      port: 8545,
+      gas: 4543760
+    },
   },
   solc: {
     optimizer: {
