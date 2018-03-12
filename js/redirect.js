0ef4343e7507f19b5540a372402a98d0bb021ba0
Matan Tsuberi <dev.matan.tsuberi>
2018-03-12 22:20:13 +0200

    Deployed f2afbaf with MkDocs version: 0.17.2

--- js/redirect.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is a simple mkdocs `extra_javascript` script that redirects
- * the user to `/README` automatically if location is `/`.
- *
- * Author: Matan Tsuberi <dev.matan.tsuberi@gmail.com>
- */
-
-if(window.location.pathname === '/')
-  window.location = 'README';
