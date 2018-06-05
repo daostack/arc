a29e62339a26ac9da6c37fdf4c48ffd59b025995
Doug Kent <github@douglaskent.com>
2018-06-05 08:50:59 -0400

    restored gas limits

--- truffle.js
+++ truffle.js
@@ -10,31 +10,31 @@ module.exports = {
       network_id: 1,
       host: "localhost",
       port: 8546,
-      gas: 4600000
+      gas: 4543760
     },
     ropsten: {
       network_id: 3,
       host: "localhost",
       port: 8545,
-      gas: 4600000
+      gas: 4543760
     },
     rinkeby: {
       network_id: 4,
       host: "localhost",
       port: 8545,
-      gas: 4600000
+      gas: 4543760
     },
     kovan: {
       network_id: 42,
       host: "localhost",
       port: 8545,
-      gas: 4600000
+      gas: 4543760
     },
     development: {
       network_id: "*",
       host: "localhost",
       port: 8545,
-      gas: 4600000
+      gas: 4543760
     },
   },
   solc: {
