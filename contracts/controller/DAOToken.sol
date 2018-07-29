f43ee3ea71a73b2abe3f7888491cfcaffaf738cc
Oren <oren.sokolowsky@gmail.com>
2018-07-29 14:18:21 +0300

    Update openzepplin-solidity v to 1.11.0

--- contracts/controller/DAOToken.sol
+++ contracts/controller/DAOToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
+import "../token/ERC827/ERC827Token.sol";
 
 
 /**
