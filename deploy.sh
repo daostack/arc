62f4207ab32cd2de9c9042d8d04db0910c44fdde
Matan Tsuberi <dev.matan.tsuberi@gmail.com>
2018-10-21 14:00:04 +0300

    revert to no auth required

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -f .api_key ]; then
-  echo "ERROR: .api_key file not found. Run 'npm run authorize' to obtain one"
-  exit 1
-else
-  graph deploy "$@" --api-key $(cat .api_key) --ipfs /ip4/127.0.0.1/tcp/5001 --node http://127.0.0.1:8020/ -n daostack subgraph.yaml
-fi
