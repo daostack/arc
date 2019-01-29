b407f477071f26f897dc72028acf0c0544ff6a89
Oren <oren.sokolowsky@gmail.com>
2019-01-29 13:47:10 +0200

    add wallet.sol

---
+++ contracts/test/Wallet.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.2;
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+contract Wallet is Ownable {
+
+    event ReceiveEther(address indexed _sender, uint256 _value);
+    event Pay(address indexed _sender, uint256 _value);
+
+    function() external payable {
+        emit ReceiveEther(msg.sender, msg.value);
+    }
+
+    function pay(address payable _beneficiary) public onlyOwner {
+        uint256 amount = address(this).balance;
+        _beneficiary.transfer(amount);
+        emit Pay(_beneficiary, amount);
+    }
+
+}
