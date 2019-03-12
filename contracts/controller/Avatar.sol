af108f1d5785748db5f7be79903769bdb6ab3d78
Oren <oren.sokolowsky@gmail.com>
2019-03-12 17:04:36 +0200

    fix event order

--- contracts/controller/Avatar.sol
+++ contracts/controller/Avatar.sol
@@ -17,7 +17,7 @@ contract Avatar is Ownable {
     DAOToken public nativeToken;
     Reputation public nativeReputation;
 
-    event GenericCall(address indexed _contract, bytes _params, bool _success, uint _value);
+    event GenericCall(address indexed _contract, bytes _data, uint _value, bool _success);
     event SendEther(uint256 _amountInWei, address indexed _to);
     event ExternalTokenTransfer(address indexed _externalToken, address indexed _to, uint256 _value);
     event ExternalTokenTransferFrom(address indexed _externalToken, address _from, address _to, uint256 _value);
@@ -56,7 +56,7 @@ contract Avatar is Ownable {
     returns(bool success, bytes memory returnValue) {
       // solhint-disable-next-line avoid-call-value
         (success, returnValue) = _contract.call.value(_value)(_data);
-        emit GenericCall(_contract, _data, success, _value);
+        emit GenericCall(_contract, _data, _value, success);
     }
 
     /**
