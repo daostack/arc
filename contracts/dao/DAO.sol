f67caee7e068b3e1d2346511bd0b944dacbe8633
Oren Sokolowsky <oren@daostack.io>
2019-12-31 10:40:53 +0200

    add dao.sol

--- contracts/dao/DAO.sol
+++ contracts/dao/DAO.sol
@@ -1,24 +1,57 @@
 pragma solidity 0.5.15;
 
-import "@daostack/infra-experimental/contracts/Reputation.sol";
-import "./DAOToken.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
-import "../libs/SafeERC20.sol";
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
 
 /**
- * @title An Avatar holds tokens, reputation and ether for a controller
+ * @title ActorsRegistry
  */
-contract DAO is Initializable, Ownable {
-    using SafeERC20 for address;
+contract ActorsRegistry is Ownable {
+
+    mapping(address=>bool) public actorsRegistry;
+
+    function registerActor(address _actor) public onlyOwner {
+        actorsRegistry[_actor] = true;
+    }
+
+    function unregisterActor(address _actor) public onlyOwner {
+        actorsRegistry[_actor] = false;
+    }
+}
+
+
+/**
+ * @title AssetsConstraintRegistery
+ */
+contract AssetsConstraintRegistery is Ownable {
+
+    //a mapping from asset to its constraint contract
+    mapping(address=>address) public assetsConstraintRegistery;
+
+    function addAssetConstraint(address _asset, address _constraint) public onlyOwner {
+        assetsConstraintRegistery[_asset] = _constraint;
+    }
+
+    function removeAssetConstraint(address _asset) public onlyOwner {
+        assetsConstraintRegistery[_asset] = address(0);
+    }
+
+    function isOkToCall(address _asset, bytes memory _data, uint256 _value) public returns(bool okToCall) {
+        // solhint-disable-next-line avoid-call-value
+        (okToCall,) = assetsConstraintRegistery[_asset].call.value(_value)(_data);
+    }
+}
+
+
+/**
+ * @title A DAO
+ */
+contract DAO is Initializable {
 
     string public orgName;
-    //
-    mapping(address=>bool) public actorsRegistery;
-    //a mapping from asset to its constraint contract
-    mapping(address=>address) public assetsConstraint;
+    ActorsRegistry public actorsRegistry;
+    AssetsConstraintRegistery public assetsConstraintRegistery;
 
 
     event GenericCall(address indexed _contract, bytes _data, uint _value, bool _success);
@@ -27,21 +60,14 @@ contract DAO is Initializable, Ownable {
     * @dev initialize takes organization name, native token and reputation system
     and creates an avatar for a controller
     */
-    function initialize(string calldata _orgName)
+    function initialize(string calldata _orgName,
+                        ActorsRegistry _actorsRegistry,
+                        AssetsConstraintRegistery _assetsConstraintRegistery)
     external
     initializer {
         orgName = _orgName;
-    }
-
-    // Modifiers:
-    modifier onlyRegisteredActors() {
-        require(actorsRegistery[msg.sender]);
-        _;
-    }
-
-    modifier onlyDAO() {
-        require(msg.sender == address(this));
-        _;
+        actorsRegistry = _actorsRegistry;
+        assetsConstraintRegistery = _assetsConstraintRegistery;
     }
 
     /**
@@ -54,24 +80,11 @@ contract DAO is Initializable, Ownable {
     */
     function genericCall(address _contract, bytes calldata _data, uint256 _value)
     external
-    onlyRegisteredActors
     returns(bool success, bytes memory returnValue) {
+        require(actorsRegistry.actorsRegistry(msg.sender), "caller is not a registered actor");
+        require(assetsConstraintRegistery.isOkToCall(_contract, _data, _value), "there is a constraint on this call");
       // solhint-disable-next-line avoid-call-value
         (success, returnValue) = _contract.call.value(_value)(_data);
         emit GenericCall(_contract, _data, _value, success);
     }
-
-    /**
-    * @dev registerActor register an actor to the dao
-    * @param _actor the actor address
-    */
-    function registerActor(address _actor)
-    external
-    onlyRegisteredActors
-    returns(bool success, bytes memory returnValue) {
-      // solhint-disable-next-line avoid-call-value
-        (success, returnValue) = _contract.call.value(_value)(_data);
-        emit GenericCall(_contract, _data, _value, success);
-    }
-
 }
