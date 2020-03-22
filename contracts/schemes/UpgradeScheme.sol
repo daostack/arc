65c8a13bbbb2a774ff26c13bacb446d027ae1b89
benk10 <ben@daostack.io>
2020-03-22 20:57:34 +0200

    Update UpgradeScheme.sol

--- contracts/schemes/UpgradeScheme.sol
+++ contracts/schemes/UpgradeScheme.sol
@@ -28,7 +28,7 @@ contract UpgradeScheme is VotingMachineCallbacks, ProposalExecuteInterface, Init
     event ProposalExecuted(
         address indexed _avatar,
         bytes32 indexed _proposalId,
-        int256 _param
+        bool _decision
     );
 
     event ProposalDeleted(address indexed _avatar, bytes32 indexed _proposalId);
@@ -104,7 +104,7 @@ contract UpgradeScheme is VotingMachineCallbacks, ProposalExecuteInterface, Init
 
         delete organizationProposals[_proposalId];
         emit ProposalDeleted(address(avatar), _proposalId);
-        emit ProposalExecuted(address(avatar), _proposalId, _decision);
+        emit ProposalExecuted(address(avatar), _proposalId, _decision == 1);
         return true;
     }
 
