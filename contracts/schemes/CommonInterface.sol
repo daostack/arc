c9921bedcfdb2ad05b9ccf32293bbde9d48f8a6f
orenyodfat <orenyodfat@users.noreply.github.com>
2020-03-25 17:23:18 +0200

    add common interface (#726)

---
+++ contracts/schemes/CommonInterface.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.16;
+
+
+contract CommonInterface {
+    string public constant FUNDED_BEFORE_DEADLINE_KEY = "FUNDED_BEFORE_DEADLINE";
+    string public constant FUNDED_BEFORE_DEADLINE_VALUE = "TRUE";
+}
