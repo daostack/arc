ca10d73a18553e18bd141a57457d0934ad9aa83a
Oren Sokolowsky <oren@daostack.io>
2020-05-19 13:08:11 +0300

    firstr version

---
+++ contracts/misc/CommonToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.17;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/StandaloneERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+
+
+/**
+ * @title CommonToken, base on zeppelin contract.
+ * @dev ERC20 compatible token. It is a mintable, burnable token(onlyOwner)
+ */
+contract CommonToken is StandaloneERC20 {
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public onlyMinter {
+        _burn(_msgSender(), amount);
+    }
+}
